928b3d8026a267a231fea67e9503843ee9f6c172
Swayne James <swaynejame11@gmail.com>
2019-12-01 06:39:02 +0000

    part 1 v1.2

--- addj.js
+++ addj.js
@@ -2,23 +2,88 @@ function validate(){
 	function clickButton() {
 		var button = document.getElementById("submitButton");
 	    //Add event listener
-	    button.addEventListener("click", function(e){
-	    	e.preventDefault();
+	    button.addEventListener("click", function(event){
+	    	var valid = false;
 	    	var xmlhttp = new XMLHttpRequest();
 	    	var firstname = document.getElementById("fname").value;
 		    var lastname = document.getElementById("lname").value;
 		    var email = document.getElementById("email").value;
 		    var password = document.getElementById("pass").value;
-	  
-	    	xmlhttp.open("POST", "addp.php");
-	    	xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-	    	xmlhttp.send("firstname="+firstname+"&lastname="+lastname+"&email="+email+"&password="+password);
-	      
-	    	xmlhttp.onreadystatechange = function() {
-	        	if (this.readyState === 4 && this.status === 200) {
-	        	console.log(this.responseText);
-	        	}
-	    	}
+		    
+		    var emailcheck=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		    var passcheck=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+		    
+		    if(firstname != ""){
+		    	var validf = true;
+		    	// document.getElementById("fname").className='null';
+		    }
+		    else{
+		    	validf=false;
+		    	document.getElementById("p1").innerHTML="Invaild firstname";
+		    	// document.getElementById("fname").className='check';
+		    }
+		    
+		    if(lastname != ""){
+		    	var validl = true;
+		    	// document.getElementById("lname").className='null';
+		    }
+		    else{
+		    	validl=false;
+		    	document.getElementById("p2").innerHTML="Invaild lastname";
+		    	// document.getElementById("lname").className='check';
+		    }
+		    
+		    if(password != ""){
+		    	if(password.match(passcheck)){
+		    		var validp = true;
+		    		// document.getElementById("pass").className='null';
+		    		// document.getElementById("passcon").className='null';
+		    	}
+		    	
+		    }
+		    else{
+		    	validp=false;
+		    	document.getElementById("p3").innerHTML="Invaild password";
+		    	// document.getElementById("pass").className='check';
+		    	// document.getElementById("passcon").className='check';
+		    }
+		    
+		    if(email != ""){
+		    	if(email.match(passcheck)){
+		    		var valide = true;
+		    		// document.getElementById("email").className='null';
+		    	}
+		    }
+		    else{
+		    	valide=false;
+		    	document.getElementById("p4").innerHTML="Invaild email";
+		    	// document.getElementById("email").className='check';
+		    }
+	 
+	    	 function myValid(){
+		    	if(validf == true && validl == true && valide == true && validp == true){
+			    	return true;
+			    }
+			    else{
+			    	return false;
+			    }
+		    }
+		    
+		    if(myValid()==false){
+		    	event.preventDefault();
+		    }
+		    else{
+		    	event.preventDefault();
+		    	xmlhttp.open("POST", "addp.php");
+		    	xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		    	xmlhttp.send("firstname="+firstname+"&lastname="+lastname+"&email="+email+"&password="+password);
+		      
+		    	xmlhttp.onreadystatechange = function() {
+		        	if (this.readyState === 4 && this.status === 200) {
+		        	console.log(this.responseText);
+		        	}
+		    	}
+		    }
 	    });//End of click button
 	}
 	clickButton();
--- addp.php
+++ addp.php
@@ -16,6 +16,60 @@ $lname = $_POST['lastname'];
 $eMail = $_POST['email'];
 $passcode = password_hash($_POST['password'],PASSWORD_DEFAULT);
 $current_date = date("Y/m/d");
+?>
+<?php
+function checkData($val){
+  $val = trim($val);
+  $val = stripslashes($val);
+  $val = htmlspecialchars($val);
+  return $val;
+}
+?>
+
+<?php
+if (empty($_POST["firstname"])){
+        echo "firstname is required";
+    } 
+    else {
+        $first = checkData($_POST["firstname"]);
+        // check name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$first)) {
+            echo "Only letters and white space allowed";
+        }
+    }
+    
+    if (empty($_POST["lastname"])){
+        echo "lastname is required";
+    } 
+    else {
+        $last = checkData($_POST["lastname"]);
+        // check name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$last)) {
+           echo "Only letters and white space allowed";
+        }
+    }
+    
+    if (empty($_POST["password"])){
+        echo "Password is required";
+    }
+    else{
+        $passcode = checkData($_POST["password"]);
+        // check if e-mail address syntax is valid or not
+        if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/",$passcode)){
+            echo "Invalid password format";
+        }
+    }
+    
+    if (empty($_POST["email"])){
+        echo "email is required";
+    }
+    else{
+        $eMail = checkData($_POST["email"]);
+        // check if e-mail address syntax is valid or not
+        if (!preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/",$eMail)){
+            echo "Invalid email format";
+        }
+    }
 
 $sql = "INSERT INTO users (firstname, lastname, password, email, date_joined) 
             VALUES('$fname', '$lname', '$passcode', '$eMail', '$current_date')";
